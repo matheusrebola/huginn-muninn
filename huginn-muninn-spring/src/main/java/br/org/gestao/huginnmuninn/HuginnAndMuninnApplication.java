9717f78d526d13740b647c03c700d321b3e5212a
Matheus Rebola <matheus.rebola@outlook.com>
2024-04-05 16:27:27 -0300

    first commit

---
+++ huginn-muninn-spring/src/main/java/br/org/gestao/huginnmuninn/HuginnAndMuninnApplication.java
@@ -0,0 +1,13 @@
+package br.org.gestao.huginnmuninn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuginnAndMuninnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HuginnAndMuninnApplication.class, args);
+	}
+
+}
